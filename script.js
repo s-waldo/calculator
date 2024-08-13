1a3f309d5b9ef32c8e5a90cefe972fc3e935a132
Stephen Waldo <stephen.waldo.94@gmail.com>
2024-08-12 22:56:04 -0500

    Create main calc functionality

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 const calculator = document.querySelector(".calculator-body");
 const display = document.getElementById("display");
 const keys = document.querySelectorAll("button");
+let valueOfFirstInput = 0;
+let valueOfSecondInput = 0;
+let functionSign = "";
+let newNumber = false;
+let calculationPerformed = false;
+
+const reset = () => {
+  valueOfFirstInput = 0;
+  valueOfSecondInput = 0;
+  display.innerText = "0";
+};
+
+const doCalculation = (sign) => {
+  const calculation = eval(valueOfFirstInput + sign + valueOfSecondInput);
+  if (calculation > 99999999) {
+    return "Error"
+  } else {
+    return Math.round(calculation * 100000) / 100000
+  }
+};
+
+const setSign = (action) => {
+  if (action === "add") {
+    functionSign = "+";
+  } else if (action === "subtract") {
+    functionSign = "-";
+  } else if (action === "divide") {
+    functionSign = "/";
+  } else {
+    functionSign = "*";
+  }
+};
 
 keys.forEach((key) => {
   key.addEventListener("click", (e) => {
     const action = key.dataset.action;
     // Action for numbers
     if (!action) {
-      console.log(e.target.innerText);
-    } 
+      if (display.innerText.length >= 8) {
+        return;
+      }
+      if (calculationPerformed) {
+        newNumber = true;
+        !calculationPerformed;
+      }
+      if (display.innerText === "0" || newNumber) {
+        display.innerText = e.target.innerText;
+        newNumber = false;
+      } else {
+        display.innerText += e.target.innerText;
+      }
+    }
     // Action for functions
     else if (
       action === "add" ||
@@ -16,27 +60,32 @@ keys.forEach((key) => {
       action === "divide" ||
       action === "multiply"
     ) {
-      console.log(e.target.innerText);
-    } 
+      valueOfFirstInput = parseFloat(display.innerText);
+      setSign(action);
+      newNumber = true;
+    }
     //Action for clearing
     else if (action === "clear") {
-      console.log(action)
-    } 
+      reset();
+    }
     // Action for decimal points
     else if (action === "decimal") {
-      console.log("float");
-    } 
+      display.innerText += ".";
+    }
     //Action for submitting
     else if (action === "calculate") {
-      console.log("Submitted!");
+      valueOfSecondInput = parseFloat(display.innerText);
+      display.innerText = doCalculation(functionSign);
     }
     // Action for percentages
     else if (action === "percent") {
-      console.log(e.target.innerText)
-    } 
+      const percent = parseFloat(display.innerText);
+      display.innerText = percent / 100;
+    }
     // Action for pos-neg functionality
     else {
-
+      const input = parseFloat(display.innerText);
+      display.innerText = input * -1;
     }
   });
 });
